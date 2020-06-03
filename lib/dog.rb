abe195b4e8bf0d97097fc4ad7500075393d6cdc9
Erin Stansbury <ekstansbury@gmail.com>
2020-06-03 03:07:18 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,52 @@
+class Dog 
+  attr_accessor :name
+  attr_reader :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT 
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table 
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db(arr)
+    Dog.new(id: arr[0],name: arr[1], breed: arr[2])
+  end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end 
+    self
+  end
+  
+  
+  
+end
